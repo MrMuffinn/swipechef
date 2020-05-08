16b236f46e036f53bacfa1a8c68aa583f3b18997
MrMuffinn <mrmuffincake232@gmail.com>
2020-05-08 11:48:28 +0200

    Initial Commit

---
+++ src/main/java/com/itann/swipechef/SwipechefApplication.java
@@ -0,0 +1,13 @@
+package com.itann.swipechef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipechefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipechefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itann/swipechef/SwipechefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itann.swipechef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipechefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
